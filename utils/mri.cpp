630750a726792179ce7e073180e3b40947861e55
Yujing Huang <yhuang43@mgh.harvard.edu>
2025-05-30 10:05:45 -0400

    change MRIcopyHeader() - print source ctab info only if Gdiag is set

--- utils/mri.cpp
+++ utils/mri.cpp
@@ -6284,7 +6284,8 @@ MRI *MRIcopyHeader(const MRI *mri_src, MRI *mri_dst)
   mri_dst->ncmds = mri_src->ncmds;
 
   if(mri_src->ct) {
-    printf("MRIcopyHeader(): source has ctab. Copying ctab ...\n");
+    if (Gdiag & DIAG_SHOW && DIAG_VERBOSE_ON)
+      printf("MRIcopyHeader(): source has ctab. Copying ctab ...\n");
     if(mri_dst->ct) CTABfree(&mri_dst->ct);
     mri_dst->ct = CTABdeepCopy(mri_src->ct);
   }
