31c9613ee9eb187d2806f81d381c0aa8ba7dd1f1
Douglas Greve <greve@nmr.mgh.harvard.edu>
2026-02-09 11:42:02 -0500

    fast_fftaxis.m Now returns fftaxisr

--- fsfast/toolbox/fast_fftaxis.m
+++ fsfast/toolbox/fast_fftaxis.m
@@ -1,17 +1,28 @@
-function [fftaxis, deltafreq, indaxis] = fast_fftaxis(Ntp,TR)
-% [fftaxis deltafreq indaxis] = fast_fftaxis(Ntp,TR)
+function [fftaxis, deltafreq, indaxis, indaxisr] = fast_fftaxis(Ntp,TR)
+% [fftaxis deltafreq indaxis indaxisr] = fast_fftaxis(Ntp,TR)
 %
 % Returns the frequencies at which the fft is computed, 
-% from DC to the nyquist frequency. There will be Ntp/2 + 1
-% frequencies.
+% from DC to the nyquist frequency. There will be Ntp/2+1
+% frequencies. The last (Ntp/2-1) will be a replication of
+% the 2:(Ntp/2) of the first section, but reversed.
 %
 % Example:
 % fftaxis_pos = fast_fftaxis(Nf,TR);
 %
 % To get the negative frequencies:
-% fftaxis_neg = -fliplr(fftaxis_pos(2:end-1));
+% fftaxis_neg = conj(fliplr(fftaxis_pos(2:end-1)));
 %
-%
+% EG, 
+% h = randn(1,128);
+% hfft = fft(h);
+% [fftaxis deltafreq indaxis indaxisr] = fast_fftaxis(128,1);
+% plot(fftaxis,abs(hfft(indaxis)))
+% 
+% If you only had the pos freq, eg,
+% a = hfft(indaxis);
+% b = conj(a(indaxisr))
+% c = [a b];
+% c will now be equal to hfft
 
 
 %
@@ -35,9 +46,11 @@ if(nargin ~= 2)
   qoe(msg); error(msg);
 end
 
-nn = 0:round(Ntp/2);
+Ntpdiv2 = round(Ntp/2);
+nn = 0:Ntpdiv2;
 freqmax = (1/TR)/2;         % Nyquist
 deltafreq = freqmax/(Ntp/2); % Measured from 0 to Nyquist
 fftaxis = deltafreq*nn;
 indaxis = nn+1;
+indaxisr = [Ntpdiv2:-1:2];
 return;
